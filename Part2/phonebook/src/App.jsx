c9b409db039b48f7e6a5d8d0f0c6e266dfde252d
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-01-23 15:32:35 +0100

    Add json server and get the data from there

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
+
+  // Set persons state from server
+  useEffect(() => {
+    // Get data from server
+    axios.get('http://localhost:3001/persons')
+      .then(response => { // Promise fulfilled
+        // Set persons state
+        setPersons(response.data)
+      })
+  }, [])
 
   const [inputs, setInputs] = useState({
     name: '',
