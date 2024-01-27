40e95d0c9305f7481311bd9731b43fbd2d25a82e
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-01-27 18:29:40 +0100

    Implemented Create person and validate the json received

--- Part3/phoneBookBackend/index.js
+++ Part3/phoneBookBackend/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// This enables the parsing of the body of the HTTP request
+app.use(express.json())
+
+
 let persons = [
   {
     "name": "Arto Hellas",
@@ -41,6 +45,28 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 })
 
+app.post('/api/persons', (request, response) => {
+  const person = {...request.body, id:generateId()}
+
+  // Validate that person has name and number
+  if (!person.name || !person.number) {
+    // Return 400 (bad request)
+    return response.status(400).json({error:"The person must have name and number"})
+  }
+
+  // Validate that person name is unique
+  const nameExists = persons.find(p => p.name === person.name)
+  if (nameExists) {
+    // Return 400 (bad request)
+    return response.status(400).json({error:"The person name must be unique"})
+  }
+
+  // Add new person
+  persons.push(person)
+
+  response.json(person);
+})
+
 app.get('/api/persons/:id', (request, response) => {
   // Get id from request
   const id = Number(request.params.id);
@@ -69,3 +95,7 @@ const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+function generateId() {
+  return Math.floor(Math.random() * (10000 - 5000 + 1) + 5000)
+}
